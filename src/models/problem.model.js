906a766422759d1f13c016d838c312ae7493c0c0
S Bhattacharya <swapnamoybhattacharya053@gmail.com>
2026-04-18 20:37:02 +0530

    Problem schema created

---
+++ src/models/problem.model.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const problemSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        enum: ["DSA", "HR", "System Design"],
+        required: true
+    },
+    difficulty: {
+        type: String,
+        enum: ["Easy", "Medium", "Hard"],
+        required: true
+    },
+    tags: [{
+        type: String
+    }],
+    constraints: String,
+    examples: [
+        {
+            input: String,
+            output: String,
+            explanation: String
+        }
+    ],
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: User,
+        required: true
+    }
+}, {timestamps: true});
+
+module.exports = mongoose.model("Problem", problemSchema);
